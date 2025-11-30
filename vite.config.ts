daf6fed6c7ed5678b99021d483475e9bec1e6e1c
nedfusion <32440074+nedfusion@users.noreply.github.com>
2025-11-30 19:31:06 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'esbuild',
+  },
+  base: './',
 });
